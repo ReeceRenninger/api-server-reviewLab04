f939af3a1877f3b58c3e20ff68cf3fe115760de5
Michael Pace <michael.g.pace@gmail.com>
2023-05-17 18:43:42 -0500

    food route added to serverjs

--- src/server.js
+++ src/server.js
@@ -2,18 +2,17 @@
 
 const express = require('express');
 const cors = require('cors');
+const foodRouter = require('./routes/food');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use(foodRouter);
 app.get('/', (req, res, next) => {
   res.status(200).send('proof: LIFE!');
 });
-
-
-
 
 
 
