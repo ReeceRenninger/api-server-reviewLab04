2ae755ae5ba1222a4b7edabb96bc72b021729db2
ReeceRenninger <renningerreece@gmail.com>
2023-05-18 15:10:07 -0700

    fixed get all route and post route, changed import syntax on foodModel

--- src/routes/food.js
+++ src/routes/food.js
@@ -3,21 +3,26 @@
 const express = require('express');
 
 const router = express.Router();
-const { foodModel } = require('../models');
+const { foodModel } = require('../models/index'); // changed import to directly go to index.js
 
+//find all food
 router.get('/food', async (req, res, next) => {
-  let foods = await  foodModel.findAll();
+  let foods = await  foodModel.findAll(); // 90 was here by accident fixed
+
   res.status(200).send(foods);
 });
 
+//find one food item
 router.get('/food/:id', async (req, res, next) => {
-  let singleFood = await foodModel.findAll({where: {id: req.params.id}}); //where clause useful for update
+  let singleFood = await foodModel.findByPk(req.params.id); //where clause useful for update
 
   res.status(200).send(singleFood);
 });
 
+//create new food item
 router.post('/food', async (req, res, next) => {
   let newFood = await foodModel.create(req.body);
+
   res.status(200).send(newFood);
 });
 
