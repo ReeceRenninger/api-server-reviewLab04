aefc898245b917807bbc5413f7d14addf752c76a
Michael Pace <michael.g.pace@gmail.com>
2023-05-18 11:57:01 -0500

    added notes to model index

--- src/models/index.js
+++ src/models/index.js
@@ -3,10 +3,13 @@
 const { Sequelize, DataTypes } = require('sequelize');
 const food = require('./food');
 
+//will make dynamic for testing enviornment
 const DATABASE_URL = process.env.NODE_ENV === 'test' ? 'sqlite::memory:' : process.env.DATABASE_URL;
 
+// db singleton
 const sequelizeDatabase = new Sequelize(DATABASE_URL);
 
+//create our working and connected food db model
 const foodModel = food(sequelizeDatabase, DataTypes);
 
 module.exports = { sequelizeDatabase, foodModel };
