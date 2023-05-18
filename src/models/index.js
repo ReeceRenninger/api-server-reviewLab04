80f5090f737cf245f1398e94763908c450a5918a
Michael Pace <michael.g.pace@gmail.com>
2023-05-18 13:46:03 -0500

    commented out clothes connections in index and server

--- src/models/index.js
+++ src/models/index.js
@@ -4,13 +4,13 @@ const { Sequelize, DataTypes } = require('sequelize');
 const food = require('./food');
 
 //will make dynamic for testing enviornment
-const DATABASE_URL = process.env.NODE_ENV === 'test' ? 'sqlite::memory:' : process.env.DATABASE_URL;
+const DATABASE_URL = process.env.NODE_ENV === 'test' ? 'sqlite::memory:' : process.env.DATABASE_URL; //double colon might not work
 
 // db singleton
 const sequelizeDatabase = new Sequelize(DATABASE_URL);
 
 //create our working and connected food db model
 const foodModel = food(sequelizeDatabase, DataTypes);
-const clothesModel = order(sequelizeDatabase, DataTypes)
+// const clothesModel = order(sequelizeDatabase, DataTypes)
 
 module.exports = { sequelizeDatabase, foodModel };
--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const cors = require('cors');
 const foodRouter = require('./routes/food');
-const clothesRouter = require('./routes/clothes')
+// const clothesRouter = require('./routes/clothes')
 
 const app = express();
 
@@ -11,12 +11,10 @@ app.use(cors());
 app.use(express.json());
 
 app.use(foodRouter);
-app.use(clothesRouter)
+// app.use(clothesRouter)
 app.get('/', (req, res, next) => {
   res.status(200).send('proof: LIFE!');
 });
-
-
 
 // app.use('*', (req, res, next) => {
 //   res.status(404).send('Not found');
